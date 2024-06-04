f8f219318dcffa6a222e5fba6d5b560ab12fc8f4
Patrick Deigan <pdeigan@gmail.com>
2024-06-04 13:44:26 -0400

    cleaning up gitignore

--- build/tests/external/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/patrickdeigan/Documents/projects/c/staxchallenge/external/gtest
-# Build directory: /Users/patrickdeigan/Documents/projects/c/staxchallenge/build/tests/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/tests/tests/CMakeFiles/DoublyLinkedListTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DoublyLinkedListTest.
-# This may be replaced when dependencies are built.
